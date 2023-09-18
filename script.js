b3961079f18d9a2832ef3e2dad7ae5612b5c54df
Inna Červenková <yes>
2023-09-19 00:42:32 +0200

    Implement dark mode functionality

--- script.js
+++ script.js
@@ -37,3 +37,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // animateImage(imageToAnimate);
 });
+
+/* DARK/LIGHT THEME */
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]',
+);
+
+const currentTheme = localStorage.getItem('theme')
+  ? localStorage.getItem('theme')
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
+
+const switchTheme = (e) => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+};
+
+toggleSwitch.addEventListener('change', switchTheme, false);
